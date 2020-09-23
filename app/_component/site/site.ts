2250adcb79c8e3dcc09d61d33432224436670a64
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-09-23 16:56:28 +0200

    lever bar

--- app/_component/site/site.ts
+++ app/_component/site/site.ts
@@ -1,11 +1,16 @@
 import Component from "../component"
 import declareComponent from "../../lib/declareComponent"
+import "./../../global"
 
 export default class Site extends Component {
-
+  private leverBar = this.q("lever-bar")
   constructor() {
     super()
 
+    for (let i = 0; i < 12; i++) {
+      let elem = ce("lever-element")
+      this.leverBar.apd(elem)
+    }
 
   }
 
